fd73ba9cbfc2e3a5628dfc4a1971f71554f88396
mithridatem <mathieumithridate@adrar-formation.com>
2026-05-27 11:00:09 +0200

    feat: add Category Model

---
+++ src/Entity/Category.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+class Category
+{
+    //Attributes
+    private int $id;
+    private string $name;
+
+    //Constructor (valeur obligatoire)
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    //Getters and Setters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    //toString
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+}
